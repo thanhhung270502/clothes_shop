a8111face468d7e0ec4dcf569a28b386380f436b
thanhhung270502 <thanhhung270502@gmail.com>
2022-07-10 15:25:05 +0700

    Route & Controller Init

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-// const route = require('./routes');
+const route = require('./routes');
 // const db = require('./config/db');
 
 // // Connect to DB
@@ -36,9 +36,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // routes init
-// route(app);
-
-app.get('/', (req, res) => res.render('home'));
+route(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
