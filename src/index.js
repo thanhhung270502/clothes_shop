79f921e9520f2c4ed1b045c60e24f10022e6ba7e
thanhhung270502 <thanhhung270502@gmail.com>
2022-07-10 09:16:08 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+//
+const path = require('path');
+const handlebars = require('express-handlebars');
+//
+const app = express();
+const port = 3000;
+
+// const route = require('./routes');
+// const db = require('./config/db');
+
+// // Connect to DB
+// db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// HTTP Logger
+// app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// routes init
+// route(app);
+
+app.get('/', (req, res) => res.send('Hello World!!'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
